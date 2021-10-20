e3e1f1be87cb62b264ddbd1ff31548358105aa51
mesocyclon <mesocyclon@gmail.com>
2021-10-20 15:19:26 +0500

    1519

---
+++ WebTest_2/Migrations/20211020101636_q.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebTest_2.Migrations
+{
+    public partial class q : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Phones",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecondName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateBirth = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhoneBooks",
+                columns: table => new
+                {
+                    PhoneId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InsertDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneBooks", x => new { x.UserId, x.PhoneId });
+                    table.ForeignKey(
+                        name: "FK_PhoneBooks_Phones_PhoneId",
+                        column: x => x.PhoneId,
+                        principalTable: "Phones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PhoneBooks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhoneBooks_PhoneId",
+                table: "PhoneBooks",
+                column: "PhoneId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhoneBooks");
+
+            migrationBuilder.DropTable(
+                name: "Phones");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- WebTest_2/Models/ErrorViewModel.cs
+++ WebTest_2/Models/ErrorViewModel.cs
@@ -18,21 +18,34 @@ namespace WebTest_2.Models
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public DateTime DateBirth { get; set; }
-        public virtual ICollection<PhoneUsers> UserId { get; set; }
+        public List<Phone> Phones { get; set; } = new List<Phone>();
+        public List<PhoneBook> PhoneBooks { get; set; } = new List<PhoneBook>();
     }
 
     public class Phone
     {
         public Guid Id { get; set; }
         public string PhoneNumber { get; set; }
-        public virtual ICollection<PhoneUsers> PhoneId { get; set; }
+        public List<User> Users { get; set; } = new List<User>();
+        public List<PhoneBook> PhoneBooks { get; set; } = new List<PhoneBook>();
     }
 
-    public class PhoneUsers
+    public class PhoneBook
     {
-        public Guid Id { get; set; }
-        public DateTime DateCr { get; set; }
+        public Guid PhoneId { get; set; }
+        public User User { get; set; }
+
+        public Guid UserId { get; set; }
+        public Phone Phone { get; set; }
+
+        public DateTime InsertDate { get; set; } // дата
     }
+
+    // public class PhoneUsers
+    // {
+    //     public Guid Id { get; set; }
+    //     public DateTime DateCr { get; set; }
+    // }
 
 
 
@@ -41,9 +54,30 @@ namespace WebTest_2.Models
     {
         public DbSet<User> Users/*»м€ таблицы*/{ get; set; }
         public DbSet<Phone> Phones/*им€ таблицы*/{ get; set; }
-        public DbSet<PhoneUsers> PhoneUsers/*им€ таблицы*/{ get; set; }
+        // public DbSet<PhoneUsers> PhoneUsers/*им€ таблицы*/{ get; set; }
 
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Phone>()
+                .HasMany(c => c.Users)
+                .WithMany(s => s.Phones)
+                .UsingEntity<PhoneBook>(
+                   j => j
+                    .HasOne(pt => pt.User)
+                    .WithMany(t => t.PhoneBooks)
+                    .HasForeignKey(pt => pt.UserId),
+                j => j
+                    .HasOne(pt => pt.Phone)
+                    .WithMany(p => p.PhoneBooks)
+                    .HasForeignKey(pt => pt.PhoneId),
+                j =>
+                {
+                    j.Property(pt => pt.InsertDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+                    j.HasKey(t => new { t.UserId, t.PhoneId });
+                    j.ToTable("PhoneBooks");
+                });
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=(local); Database=Site_2; Persist Security Info=false; MultipleActiveResultSets=True; Trusted_Connection=True;");
